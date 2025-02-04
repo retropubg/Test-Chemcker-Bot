ec1c2e3fc6b812a15e084aacb71294ca6bf94309
Retro <74841742+retropubg@users.noreply.github.com>
2025-02-04 17:10:55 -0400

    Update core.php

--- Core/core.php
+++ Core/core.php
@@ -1,6 +1,6 @@
 <?php
 
-$botToken =  "5980177112:AAF2saZaYIej03yT5_UE3Gbjlpgi1g7tnyw";
+$botToken =  "7851924299:AAEj70SJUOzeS5w-GHUpUiK-GiPV1CHRaSE";
 $website = "https://api.telegram.org/bot".$botToken;
 $update = file_get_contents('php://input');
 $update = json_decode($update, TRUE);
